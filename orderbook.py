b5e0075786d7a2d128524d86c05ecdab5e16af58
S.Berkebile <samuel.berkebile@gmail.com>
2025-10-14 16:55:50 -0500

    work on orderbook.py

--- orderbook.py
+++ orderbook.py
@@ -1,4 +1,13 @@
 
 
 class OrderBook:
-    pass
+    
+    def __init__(self):
+        self.bids = {} # UID, value, num_shares
+        self.asks = {}
+
+    
+    def make_bid(self, uid: str, value: float, num_shares: int):
+        if uid in self.bids:
+            raise ValueError("Bid with this UID already exists.")
+        self.bids[uid] = (value, num_shares)
